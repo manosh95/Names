c36f2af2de39da78c12ddbc3995e37b991b23f4d
manosh95 <manosh95manu@gmail.com>
2021-03-11 18:34:01 +0530

    ReverseName

---
+++ src/com/tcs/Names.java
@@ -0,0 +1,16 @@
+package com.tcs;
+
+public class Names{
+	public static void main(String[] args) {
+		Names nm = new Names();
+			nm.manosh();
+		nm.suresh();
+	}
+	private void manosh() {
+		System.out.println("manosh");
+	}
+private void suresh() {
+	System.out.println("suresh");
+
+}
+}
